eef48db5ffacc8708d75afd904c310c034d6c271
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2020-12-20 10:57:49 +0530

    Update dynamicResources.js

--- Javascript/dynamicResources.js
+++ Javascript/dynamicResources.js
@@ -1,35 +1,28 @@
-// script to get DynamicURLs. May need to break down into Coursera / Piazza
+// script to get DynamicURLs.
 
 var month = (new Date()).getMonth(); // gets month, 0 is january
 var year = (new Date().getFullYear()); // gets full year
 var day = (new Date().getDay()); // gets day, 0 is Sunday
 var term;
 
-/*
-  NOTE: It seems this needs to be updated every year! Some terms may fall between months
-  2020 schedule:
-  Spring: Jan 15 - April 26
-  Summer: May 12 - August 23 (on campus)
-  Fall: Sep 1 - Dec 20
-*/
 switch (month) {
   case 0: // jan
   case 1: // feb
   case 2: // mar
   case 3: // apr
-    term = "spring";
+    term = "semester 1";
     break;
   case 4: // may
   case 5: // jun
   case 6: // jul
-    term = "summer";
+    term = "semester 2";
     break;
   case 7: //aug
   case 8: //sept
   case 9: //oct
   case 10: //nov
   case 11: //dec
-    term = "fall";
+    term = "semester 3";
     break;
 }
 
